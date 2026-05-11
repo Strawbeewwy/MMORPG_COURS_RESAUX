083199a4e119bf84d67f54198c586fd2e8ad8447
Strawbeewwy <Strawbeewwy@gmail.com>
2026-05-11 15:07:18 -0400

    More comments to understand the way it works

--- Launcher/src/config.rs
+++ Launcher/src/config.rs
@@ -1,3 +1,10 @@
+/**
+config.rs contains all constants that are used throughout
+the application. We use constants to make it easier to change
+the configuration of the application. For example, we could
+change the address of the gatekeeper server here.
+**/
+
 pub const GATEKEEPER_ADDRESS: &str = "127.0.0.1:4000";
 pub const GATEKEEPER_SERVER_NAME: &str = "localhost";
 pub const LAUNCHER_VERSION: &str = "0.1.0";
